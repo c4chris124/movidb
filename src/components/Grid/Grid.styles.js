27f00068c86d54f129f180f92a372f56138140b0
c4chris124 <c4chris124@gmail.com>
2021-09-18 18:11:50 -0600

    Grid iniciated

---
+++ src/components/Grid/Grid.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    max-width: var(--maxWidth);
+    margin: 0 auto;
+`;
+
+export const Content = styled.div`
+
+`;
+
