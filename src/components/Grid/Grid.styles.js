570b80a21f551bd4850797f2e95a721b688fb597
c4chris124 <c4chris124@gmail.com>
2021-09-21 15:24:55 -0600

    Grid styles

--- src/components/Grid/Grid.styles.js
+++ src/components/Grid/Grid.styles.js
@@ -3,6 +3,11 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     max-width: var(--maxWidth);
     margin: 0 auto;
+    padding: 0 20px;
+
+    h1{
+        color: var(--meGrey);
+    }
 `;
 
 export const Content = styled.div`
